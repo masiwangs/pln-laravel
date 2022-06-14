a587e469a8b75b2dafd2d4633ed514d96581bb8a
masiwangs <ichwan@sociabuzz.com>
2022-06-14 19:54:01 +0700

    update title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Table - Mazer Admin Dashboard</title>
+    <title>Monitoring</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="/assets/css/bootstrap.css">
 
